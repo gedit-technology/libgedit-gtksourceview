c2f2157a3f971eaded4a648f75f37672ad15cfe5
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-04-18 16:00:00 +0200

    StyleScheme: minor changes

--- gtksourceview/gtksourcestylescheme.h
+++ gtksourceview/gtksourcestylescheme.h
@@ -1,5 +1,4 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8; coding: utf-8 -*-
- *
+/*
  * This file is part of GtkSourceView
  *
  * Copyright (C) 2003 - Paolo Maggi <paolo.maggi@polito.it>
@@ -13,7 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
-
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
@@ -38,25 +37,25 @@ G_BEGIN_DECLS
 #define GTK_SOURCE_IS_STYLE_SCHEME_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_SOURCE_TYPE_STYLE_SCHEME))
 #define GTK_SOURCE_STYLE_SCHEME_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_SOURCE_TYPE_STYLE_SCHEME, GtkSourceStyleSchemeClass))
 
-typedef struct _GtkSourceStyleSchemePrivate      GtkSourceStyleSchemePrivate;
-typedef struct _GtkSourceStyleSchemeClass        GtkSourceStyleSchemeClass;
+typedef struct _GtkSourceStyleSchemeClass	GtkSourceStyleSchemeClass;
+typedef struct _GtkSourceStyleSchemePrivate	GtkSourceStyleSchemePrivate;
 
 struct _GtkSourceStyleScheme
 {
-	GObject base;
+	GObject parent;
 	GtkSourceStyleSchemePrivate *priv;
 };
 
 struct _GtkSourceStyleSchemeClass
 {
-	GObjectClass base_class;
+	GObjectClass parent_class;
 
 	/* Padding for future expansion */
 	gpointer padding[10];
 };
 
 G_MODULE_EXPORT
-GType			 gtk_source_style_scheme_get_type			(void) G_GNUC_CONST;
+GType			 gtk_source_style_scheme_get_type			(void);
 
 G_MODULE_EXPORT
 const gchar             *gtk_source_style_scheme_get_id				(GtkSourceStyleScheme *scheme);
@@ -78,7 +77,7 @@ GtkSourceStyle		*gtk_source_style_scheme_get_style			(GtkSourceStyleScheme *sche
 										 const gchar          *style_id);
 
 G_GNUC_INTERNAL
-GtkSourceStyleScheme	*_gtk_source_style_scheme_new_from_file			(const gchar          *filename);
+GtkSourceStyleScheme	*_gtk_source_style_scheme_new_from_file			(const gchar *filename);
 
 G_GNUC_INTERNAL
 GtkSourceStyleScheme	*_gtk_source_style_scheme_get_default			(void);
