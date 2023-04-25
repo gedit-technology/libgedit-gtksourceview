997789fc6462a40168b5745e97de9d87be1fcdcb
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-04-25 16:00:00 +0200

    StyleScheme parser: create source files (skeleton)

---
+++ gtksourceview/gtksourcestyleschemeparser.h
@@ -0,0 +1,29 @@
+/*
+ * This file is part of GtkSourceView
+ *
+ * Copyright (C) 2023 - Sébastien Wilmet <swilmet@gnome.org>
+ *
+ * GtkSourceView is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * GtkSourceView is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GTK_SOURCE_STYLE_SCHEME_PARSER_H
+#define GTK_SOURCE_STYLE_SCHEME_PARSER_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif /* GTK_SOURCE_STYLE_SCHEME_PARSER_H */
