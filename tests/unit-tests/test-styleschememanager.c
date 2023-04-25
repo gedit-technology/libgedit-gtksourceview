72ee8911120fc2a5a2d4d0ae83169852acddc9ac
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-04-25 16:00:00 +0200

    unit-tests: styleschememanager: improve existing test cases

--- tests/unit-tests/test-styleschememanager.c
+++ tests/unit-tests/test-styleschememanager.c
@@ -2,6 +2,7 @@
  * This file is part of GtkSourceView
  *
  * Copyright (C) 2013 - Paolo Borelli
+ * Copyright (C) 2023 - Sébastien Wilmet
  *
  * GtkSourceView is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,46 +21,82 @@
 #include <gtksourceview/gtksource.h>
 
 static void
-test_get_default (void)
-{
-	GtkSourceStyleSchemeManager *default_manager1;
-	GtkSourceStyleSchemeManager *default_manager2;
-
-	default_manager1 = gtk_source_style_scheme_manager_get_default ();
-	default_manager2 = gtk_source_style_scheme_manager_get_default ();
-	g_assert_true (default_manager1 == default_manager2);
-}
-
-static void
-test_prepend_search_path (void)
-{
-	GtkSourceStyleSchemeManager *manager;
-	gchar *dataset_dir;
-	GtkSourceStyleScheme *scheme;
-	const gchar *obtained_filename;
-	gchar *expected_filename;
-
-	manager = gtk_source_style_scheme_manager_get_default ();
-
-	dataset_dir = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "basics", NULL);
-	gtk_source_style_scheme_manager_prepend_search_path (manager, dataset_dir);
-
-	scheme = gtk_source_style_scheme_manager_get_scheme (manager, "classic");
-	obtained_filename = gtk_source_style_scheme_get_filename (scheme);
-	expected_filename = g_build_filename (dataset_dir, "classic.xml", NULL);
-	g_assert_cmpstr (obtained_filename, ==, expected_filename);
-
-	g_free (dataset_dir);
-	g_free (expected_filename);
-}
-
-static void
 set_single_search_path (GtkSourceStyleSchemeManager *manager,
 			const gchar                 *path)
 {
 	const gchar *search_path[] = { path, NULL };
 
 	gtk_source_style_scheme_manager_set_search_path (manager, (gchar **) search_path);
+}
+
+/* To have reproducible results.
+ * The unit tests can be run without the need to install the project.
+ */
+static GtkSourceStyleSchemeManager *
+create_manager (void)
+{
+	GtkSourceStyleSchemeManager *manager;
+	gchar *dir;
+
+	manager = gtk_source_style_scheme_manager_new ();
+
+	dir = g_build_filename (TOP_SRCDIR, "data", "style-schemes", "default-style-schemes", NULL);
+	set_single_search_path (manager, dir);
+	g_free (dir);
+
+	return manager;
+}
+
+static void
+test_get_default (void)
+{
+	GtkSourceStyleSchemeManager *default_manager1;
+	GtkSourceStyleSchemeManager *default_manager2;
+	GtkSourceStyleSchemeManager *other_manager;
+
+	default_manager1 = gtk_source_style_scheme_manager_get_default ();
+	default_manager2 = gtk_source_style_scheme_manager_get_default ();
+	g_assert_true (default_manager1 == default_manager2);
+
+	other_manager = gtk_source_style_scheme_manager_new ();
+	g_assert_true (default_manager1 != other_manager);
+	g_object_unref (other_manager);
+}
+
+static void
+test_prepend_search_path (void)
+{
+	GtkSourceStyleSchemeManager *manager;
+	GtkSourceStyleScheme *scheme;
+	gchar *obtained_filename_before;
+	const gchar *obtained_filename_after;
+	gchar *dataset_dir;
+	gchar *expected_filename;
+
+	manager = create_manager ();
+
+	// Currently, only the default style schemes are loaded.
+	scheme = gtk_source_style_scheme_manager_get_scheme (manager, "classic");
+	g_assert_true (scheme != NULL);
+	obtained_filename_before = g_strdup (gtk_source_style_scheme_get_filename (scheme));
+
+	// Now prepend the basics/ dir to the search path.
+	dataset_dir = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "basics", NULL);
+	gtk_source_style_scheme_manager_prepend_search_path (manager, dataset_dir);
+
+	scheme = gtk_source_style_scheme_manager_get_scheme (manager, "classic");
+	g_assert_true (scheme != NULL);
+
+	obtained_filename_after = gtk_source_style_scheme_get_filename (scheme);
+	g_assert_cmpstr (obtained_filename_after, !=, obtained_filename_before);
+
+	expected_filename = g_build_filename (dataset_dir, "classic.xml", NULL);
+	g_assert_cmpstr (obtained_filename_after, ==, expected_filename);
+
+	g_object_unref (manager);
+	g_free (obtained_filename_before);
+	g_free (dataset_dir);
+	g_free (expected_filename);
 }
 
 static void
