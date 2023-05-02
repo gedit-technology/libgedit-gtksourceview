b79c5a26b5616020633367c56eb1edacaf2f19fe
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-05-02 16:00:00 +0200

    Style: remove useless #include

--- gtksourceview/gtksourcestyle.c
+++ gtksourceview/gtksourcestyle.c
@@ -17,10 +17,6 @@
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include "gtksourcestyle.h"
 #include "gtksourcestyle-private.h"
 
