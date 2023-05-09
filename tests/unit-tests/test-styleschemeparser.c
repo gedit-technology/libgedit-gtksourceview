aa28edd6aeb5feeed4767b8f6e905e535a9c1375
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-05-09 16:00:00 +0200

    unit-tests: start to test _gtk_source_style_scheme_parser_parse_file()

--- tests/unit-tests/test-styleschemeparser.c
+++ tests/unit-tests/test-styleschemeparser.c
@@ -61,6 +61,73 @@ test_parse_scale (void)
 	g_assert_false (_gtk_source_style_scheme_parser_parse_scale (" x-large", &val));
 }
 
+static void
+check_parsing_error_full (const gchar *filename,
+			  GQuark       expected_error_domain)
+{
+	gchar *path;
+	GFile *file;
+	GError *error = NULL;
+	gboolean ok;
+
+	path = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "parsing-errors", filename, NULL);
+	file = g_file_new_for_path (path);
+
+	ok = _gtk_source_style_scheme_parser_parse_file (file, NULL, NULL, &error);
+	g_assert_false (ok);
+	g_assert_nonnull (error);
+	g_assert_true (error->domain == expected_error_domain);
+
+	g_free (path);
+	g_object_unref (file);
+	g_error_free (error);
+}
+
+static void
+check_parsing_error (const gchar *filename)
+{
+	check_parsing_error_full (filename, G_MARKUP_ERROR);
+}
+
+static void
+check_successful_parsing (void)
+{
+	gchar *path;
+	GFile *file;
+	GError *error = NULL;
+	gboolean ok;
+	GtkSourceStyleSchemeBasicInfos *basic_infos = NULL;
+
+	path = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "basics", "classic.xml", NULL);
+	file = g_file_new_for_path (path);
+
+	ok = _gtk_source_style_scheme_parser_parse_file (file, &basic_infos, NULL, &error);
+	g_assert_true (ok);
+	g_assert_null (error);
+	g_assert_cmpstr (basic_infos->id, ==, "classic");
+	g_assert_cmpstr (basic_infos->name, ==, "Classic");
+
+	g_free (path);
+	g_object_unref (file);
+	_gtk_source_style_scheme_basic_infos_free (basic_infos);
+}
+
+static void
+test_parse_file (void)
+{
+	check_parsing_error_full ("file-not-found", G_IO_ERROR);
+	check_parsing_error ("001-no-style-scheme-attributes.xml");
+	check_parsing_error ("002-nested-style-scheme-elements.xml");
+	check_parsing_error ("003-two-style-scheme-elements.xml");
+	check_parsing_error ("004-empty-id.xml");
+	check_parsing_error ("005-no-name.xml");
+	check_parsing_error ("006-empty-names.xml");
+	check_parsing_error ("007-two-names.xml");
+	check_parsing_error ("008-two-descriptions.xml");
+
+	check_successful_parsing ();
+}
+
 int
 main (int    argc,
       char **argv)
@@ -72,6 +139,7 @@ main (int    argc,
 
 	g_test_add_func ("/StyleSchemeParser/parse_final_color", test_parse_final_color);
 	g_test_add_func ("/StyleSchemeParser/parse_scale", test_parse_scale);
+	g_test_add_func ("/StyleSchemeParser/parse_file", test_parse_file);
 
 	ret = g_test_run ();
 	gtk_source_finalize ();
