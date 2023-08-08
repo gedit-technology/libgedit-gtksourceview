d2cdfde3300a731e909fbdef34d2df2998873d85
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-08-08 16:00:00 +0200

    style-schemes: remove unit tests for the parent-scheme attribute

--- tests/unit-tests/test-styleschememanager.c
+++ tests/unit-tests/test-styleschememanager.c
@@ -149,92 +149,6 @@ test_append_search_path (void)
 	g_free (dataset_dir);
 }
 
-static void
-test_unknown_parent (void)
-{
-	GtkSourceStyleSchemeManager *manager;
-	gchar *dataset_dir;
-	GList *schemes;
-
-	manager = gtk_source_style_scheme_manager_new ();
-
-	dataset_dir = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "unknown-parent", NULL);
-	set_single_search_path (manager, dataset_dir);
-	g_free (dataset_dir);
-
-	g_test_expect_message (G_LOG_DOMAIN,
-			       G_LOG_LEVEL_WARNING,
-			       "*Unknown parent-scheme*");
-	schemes = gtk_source_style_scheme_manager_get_schemes (manager);
-	g_test_assert_expected_messages ();
-
-	g_assert_cmpuint (g_list_length (schemes), ==, 0);
-	g_object_unref (manager);
-}
-
-static void
-test_parents_ref_cycle (void)
-{
-	GtkSourceStyleSchemeManager *manager;
-	gchar *dataset_dir;
-	GList *schemes;
-
-	manager = gtk_source_style_scheme_manager_new ();
-
-	dataset_dir = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "parents-ref-cycle", NULL);
-	set_single_search_path (manager, dataset_dir);
-	g_free (dataset_dir);
-
-	g_test_expect_message (G_LOG_DOMAIN,
-			       G_LOG_LEVEL_WARNING,
-			       "*parent-scheme reference cycle*");
-	schemes = gtk_source_style_scheme_manager_get_schemes (manager);
-	g_test_assert_expected_messages ();
-
-	g_assert_cmpuint (g_list_length (schemes), ==, 0);
-	g_object_unref (manager);
-}
-
-static void
-test_parents (void)
-{
-	GtkSourceStyleSchemeManager *manager;
-	gchar *dataset_dir;
-	GList *schemes;
-	GtkSourceStyleScheme *scheme_classic;
-	GtkSourceStyleScheme *scheme_base;
-	GtkSourceStyleScheme *scheme_child;
-	GtkSourceStyleScheme *scheme_grand_child;
-
-	manager = gtk_source_style_scheme_manager_new ();
-
-	dataset_dir = g_test_build_filename (G_TEST_DIST, "datasets", "style-schemes", "parents", NULL);
-	set_single_search_path (manager, dataset_dir);
-	g_free (dataset_dir);
-
-	schemes = gtk_source_style_scheme_manager_get_schemes (manager);
-	g_assert_cmpuint (g_list_length (schemes), ==, 3);
-	g_list_free (schemes);
-
-	// A single search path is set, and it doesn't include the classic scheme.
-	scheme_classic = gtk_source_style_scheme_manager_get_scheme (manager, "classic");
-	g_assert_true (scheme_classic == NULL);
-
-	scheme_base = gtk_source_style_scheme_manager_get_scheme (manager, "base");
-	scheme_child = gtk_source_style_scheme_manager_get_scheme (manager, "child");
-	scheme_grand_child = gtk_source_style_scheme_manager_get_scheme (manager, "grand-child");
-
-	g_assert_true (scheme_base != NULL);
-	g_assert_true (scheme_child != NULL);
-	g_assert_true (scheme_grand_child != NULL);
-
-	g_assert_true (_gtk_source_style_scheme_get_parent_id (scheme_base) == NULL);
-	g_assert_cmpstr (_gtk_source_style_scheme_get_parent_id (scheme_child), ==, "base");
-	g_assert_cmpstr (_gtk_source_style_scheme_get_parent_id (scheme_grand_child), ==, "child");
-
-	g_object_unref (manager);
-}
-
 int
 main (int    argc,
       char **argv)
@@ -247,9 +161,6 @@ main (int    argc,
 	g_test_add_func ("/StyleSchemeManager/get_default", test_get_default);
 	g_test_add_func ("/StyleSchemeManager/prepend_search_path", test_prepend_search_path);
 	g_test_add_func ("/StyleSchemeManager/append_search_path", test_append_search_path);
-	g_test_add_func ("/StyleSchemeManager/unknown_parent", test_unknown_parent);
-	g_test_add_func ("/StyleSchemeManager/parents_ref_cycle", test_parents_ref_cycle);
-	g_test_add_func ("/StyleSchemeManager/parents", test_parents);
 
 	ret = g_test_run ();
 	gtk_source_finalize ();
